ac5d600d606b7473423f0f9463364dd0d0f0c748
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-10-02 11:08:55 -0400

    File sorting + BASH files
    I have sorted the module assignments to separate my work from the actual dataman project.

---
+++ Module Assignments/Module 1/M1T1_Jones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// CTS 285
+// M1T1
+// Kent Jones
+// 8/23/23
+namespace M1T1_Orientation_Jones
+{
+    internal class M1T1_Jones
+    {
+        static void Main(string[] args)
+        {
+            // Inputting lambda expression
+            Action<string> Display = str => Console.WriteLine(str);
+
+            //Typing out general information, interests, and other info about myself
+            Display("Hello! My name is Kent Jones Jr!");
+            Console.WriteLine();
+            Display("Currently. I am learning C++ with professor Norris.");
+            Console.WriteLine();
+            Display("Some of my hobbies include writing stories, going on jogs, and playing video games.");
+            Console.WriteLine();
+            Display("During my college journey, I have learned C#, C++, JavaScript, HTML, Python, SQL, and Oracle Databasing." + "\n" + 
+                "Out of all of these languages, I enjoy programming in Java and C# the most.");
+            Console.WriteLine();
+            Display("As for my partner for the first HW assignment, I did not catch their name.");
+
+            //Getting user input
+            Console.Write("*Press enter to end this interaction*");
+            string exitInput = Console.ReadLine();
+        }
+    }
+}
