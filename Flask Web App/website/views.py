fb898fc89b1cc01901a9340092c9630726370b03
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-10-05 21:58:45 -0400

    Added GET/POST methods and flash warnings
    In this push, I've added GET and POST methods for the sign up and login pages  and included flash messages for each field that does not meet certain requirements.

--- Flask Web App/website/views.py
+++ Flask Web App/website/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
-@views.route('/') # ('/', methods=['GET','POST']) but im paraphrasing
+@views.route('/')
 def home():
     return render_template("home.html")
