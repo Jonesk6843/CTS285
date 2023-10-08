9e144fe48d1d43d005c4a973aa778f11cc966bf2
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-10-08 16:17:16 -0400

    Theme update
    I have updated the layout of the webpage and created a logo for Calculatron.

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -4,11 +4,15 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", userText="Testing")
+    return render_template("login.html")
 
-@auth.route('/logout')
-def logout():
-    return "<p>Logout</p>"
+@auth.route('/about')
+def about():
+    return render_template("about.html")
+
+@auth.route('/applications')
+def applications():
+    return render_template("applications.html")
 
 @auth.route('/signUp', methods=['GET', 'POST'])
 def sign_up():
@@ -29,7 +33,7 @@ def sign_up():
             flash('Passwords don\'t match!', category ='error')
             pass
         elif len(password) < 7:
-            flash('EPassword must be at least 7 characters', category ='error')
+            flash('Password must be at least 7 characters', category ='error')
             pass
         else:
             # add user to database
