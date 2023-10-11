38ea7ba1640c7b70bed422fb7434220456189393
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-10-11 12:43:07 -0400

    Solved Issue #5 & Issue #5
    I have solved the conversion bug for the division application and added proper repeating for all programs. I also changed the layout of each app's if structure.
    [If correct, else EEE, If wrong==3]

--- Dataman_Applications/Jonesk_AnswerChecker.py
+++ Dataman_Applications/Jonesk_AnswerChecker.py
@@ -13,21 +13,32 @@ def addition():
   num2 = random.randint(0, 9) # Num ranged between 0 and 20
   sum = num1 + num2
   wrongGuess = 0 # Tracks user guesses
-  repeat = False # While loop key
+  appRepeat = True # While loop key
   
   # Check if the guess is correct
-  while repeat == False: 
+  while appRepeat == True: 
     print(num1, ' + ', num2, ' = ', '?')
     guess = int(input('What is the answer?: '))
-    if wrongGuess == 3: # After third strike, display answer
-        print('The Correct answer is ', sum)
-        repeat = True
     if guess == sum:
       print('C O R R E C T')
-      repeat = True
+      appRepeat = False
     else:
       print('E E E E')
       wrongGuess += 1
+    if wrongGuess == 3: # After third strike, display answer
+        print('The Correct answer is ', sum)
+        appRepeat = False
+     
+  # Prompt the user if they would like to repeat the program
+  print("1. RETRY\n2. MAIN MENU")
+  user_input = int(input("> "))
+  if user_input == 1:
+    addition()
+  elif user_input == 2:
+    main()
+  else:
+      print("ENTER VALID OPTION! LEAVING APPLICATION...")
+      main()  
       
 # Defining Subtraction
 def subtraction():
@@ -36,21 +47,32 @@ def subtraction():
   num2 = random.randint(0, 9) # Num ranged between 0 and 20
   difference = num1 - num2
   wrongGuess = 0 # Tracks user guesses
-  repeat = False # While loop key
+  appRepeat = True # While loop key
   
   # Check if the guess is correct
-  while repeat == False: 
+  while appRepeat == True: 
     print(num1, ' - ', num2, ' = ', '?')
     guess = int(input('What is the answer?: '))
-    if wrongGuess == 3: # After third strike, display answer
-        print('The Correct answer is ', difference)
-        repeat = True
     if guess == difference:
       print('C O R R E C T')
-      repeat = True
+      appRepeat = False
     else:
       print('E E E E')
       wrongGuess += 1
+    if wrongGuess == 3: # After third strike, display answer
+        print('The Correct answer is ', difference)
+        appRepeat = False
+        
+  # Prompt the user if they would like to repeat the program
+  print("1. RETRY\n2. MAIN MENU")
+  user_input = int(input("> "))
+  if user_input == 1:
+    subtraction()
+  elif user_input == 2:
+    main()
+  else:
+      print("ENTER VALID OPTION! LEAVING APPLICATION...")
+      main()  
 
 # Defining Multiplication
 def multiplication():
@@ -59,54 +81,70 @@ def multiplication():
   num2 = random.randint(0, 9) # Num ranged between 0 and 20
   product = num1 * num2
   wrongGuess = 0 # Tracks user guesses
-  repeat = False # While loop key
+  appRepeat = True # While loop key
   
   # Check if the guess is correct
-  while repeat == False: 
+  while appRepeat == True: 
     print(num1, ' * ', num2, ' = ', '?')
     guess = int(input('What is the answer?: '))
-    if wrongGuess == 3: # After third strike, display answer
-        print('The Correct answer is ', product)
-        repeat = True
     if guess == product:
       print('C O R R E C T')
-      repeat = True
+      appRepeat = False
     else:
       print('E E E E')
       wrongGuess += 1
-
+    if wrongGuess == 3: # After third strike, display answer
+        print('The Correct answer is ', product)
+        appRepeat = False
+  print("1. RETRY\n2. MAIN MENU")
+  user_input = int(input("> "))
+  if user_input == 1:
+    multiplication()
+  elif user_input == 2:
+    main()
+  else:
+      print("ENTER VALID OPTION! LEAVING APPLICATION...")
+      main()    
+    
 # Defining Division
 def division():
   print('DIVISION')
   num1 = random.randint(0, 9) # Num ranged between 0 and 20
   num2 = random.randint(0, 9) # Num ranged between 0 and 20
-  quotiant = num1 / num2
+  quotiant = float(str(round(num1 / num2, 2))) # Rounding quotiant to 2 decimal places
+
   wrongGuess = 0 # Tracks user guesses
-  repeat = False # While loop key
+  appRepeat = True # While loop key
   
   # Check if the guess is correct
-  while repeat == False: 
+  while appRepeat == True: 
     print(num1, ' / ', num2, ' = ', '?')
-    guess = int(input('What is the answer?: '))
-    if wrongGuess == 3: # After third strike, display answer
-        print('The Correct answer is ', quotiant)
-        repeat = True
+    guess = float(input('What is the answer?: '))
     if guess == quotiant:
       print('C O R R E C T')
-      repeat = True
+      appRepeat = False
     else:
       print('E E E E')
       wrongGuess += 1
-
-def exit_program():
-  print("Exiting the program...")
+    if wrongGuess == 3: # After third strike, display answer
+        print('The Correct answer is ', quotiant)
+        appRepeat = False
+        
+  # Prompt the user if they would like to repeat the program
+  print("1. RETRY\n2. MAIN MENU")
+  user_input = int(input("> "))
+  if user_input == 1:
+    division()
+  elif user_input == 2:
+    main()
+  else:
+      print("ENTER VALID OPTION! LEAVING APPLICATION...")
+      main()  
   
 # Main Menu
 def main():
   repeat = False  # While loop key
   while repeat == False:
-    print('WELCOME TO DATAMAN!')
-    print('WHAT MATHEMATICAL APPLICATIION WOULD YOU LIKE TO RUN?')
     print('WELCOME TO DATAMAN!')
     print('WHAT MATHEMATICAL APPLICATIION WOULD YOU LIKE TO RUN?')
     print('1. ADDITION')
@@ -128,7 +166,8 @@ def main():
       division()
       repeat = True
     if userInput == 5:
-      exit_program()
+      print('Exiting Program...')
+      exit(0)
     else:
       print('INVALID INPUT!')
       
