9641f49f82f9b6410d549c6d395b3704b804e7f7
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-11-06 21:35:50 -0500

    Fixed Database + Sprint Retrospect
    Tasked completed
    -Sign up page sends info to sql database
    --Autoincrament of ID not working.
    -login page checks database to match input
    -completed sprint Retrospect

--- Flask Web App/website/auth.py
+++ Flask Web App/website/auth.py
@@ -1,11 +1,29 @@
 import random
 from flask import Blueprint, render_template, request, flash
+import sqlite3
 
 auth = Blueprint('auth', __name__)
 
 # Login Page
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Connect to the database
+        conn = sqlite3.connect('userDatabase.db')
+        cur = conn.cursor()
+        cur.execute("SELECT id, email, userPass FROM users WHERE email = ?", (email,))
+        user = cur.fetchone()
+        if user is None:
+                flash('User not found. Please check your email.', 'error')
+        elif user[2] != password:
+                flash('Incorrect password. Please try again.', 'error')
+        else:
+                flash('Logged in successfully!', 'success')
+                render_template("home.html")
+        conn.close()
     return render_template("login.html")
 
 #SignUp page
@@ -19,19 +37,25 @@ def sign_up():
 
         # String input validations
         if len(email) < 4:
-            flash('Email must be greater than 3 characters!', category ='error')
-            pass
+            flash('Email must be greater than 3 characters!', category='error')
         elif len(firstName) < 2:
-            flash('Email must be greater than 1 characters!', category ='error')
-            pass
+            flash('First Name must be greater than 1 character!', category='error')
         elif password != passwordCon:
-            flash('Passwords don\'t match!', category ='error')
-            pass
+            flash('Passwords don\'t match!', category='error')
         elif len(password) < 7:
-            flash('Password must be at least 7 characters', category ='error')
-            pass
+            flash('Password must be at least 7 characters', category='error')
         else:
-            # add user to database
+            # Connect to the database
+            conn = sqlite3.connect('userDatabase.db')
+            cur = conn.cursor()
+
+            # Insert the user information into the database
+            cur.execute("INSERT INTO users (firstname, email, userPass) VALUES (?, ?, ?)", (firstName, email, password))
+
+            # Commit the changes and close the connection
+            conn.commit()
+            conn.close()
+
             flash('Account created!', category='success')
     return render_template("signUp.html")
 
